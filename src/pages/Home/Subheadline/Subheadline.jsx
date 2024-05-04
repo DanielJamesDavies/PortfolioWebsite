fdeb5d1ebaa82ad4423fd312c29aa90972493991
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2024-05-04 17:13:00 +0100

    Header Bug Fix

--- src/pages/Home/Subheadline/Subheadline.jsx
+++ src/pages/Home/Subheadline/Subheadline.jsx
@@ -3,11 +3,14 @@ import "./Subheadline.css";
 export const Subheadline = () => {
 	return (
 		<div className='home-subheadline'>
-			Computer Science (Artificial Intelligence) BSc with First Class Honours
-			<div className='home-subheadline-line-break' />
-			Predicted Distinction in Artificial Intelligence MSc
-			<div className='home-subheadline-line-break' />
-			Currently Working on Novel Methods for Increasing DL Interpretability and Capabilities
+			<div className='home-subheadline-text'>
+				<div className='home-subheadline-line-break' />
+				Predicted Distinction in Artificial Intelligence MSc
+				<div className='home-subheadline-line-break' />
+				Currently Working on Novel Methods for Increasing DL Interpretability and Capabilities
+				<div className='home-subheadline-line-break' />
+				Computer Science (Artificial Intelligence) BSc with First Class Honours
+			</div>
 		</div>
 	);
 };
