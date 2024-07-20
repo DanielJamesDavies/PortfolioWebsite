0f7f4d852eb8822c04be2bd38d6a29c881ffaaac
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2024-07-20 02:24:24 +0100

    Improvements

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -9,6 +9,48 @@ export const Page = ({ element }) => {
 			<Header />
 			<div className='page-content'>{element}</div>
 			<Footer />
+			<div className='page-background'>
+				<div className='page-background-emojis'>
+					<div className='page-background-emoji-container'>
+						<div className='page-background-emoji'>🌌</div>
+					</div>
+					<div className='page-background-emoji-container'>
+						<div className='page-background-emoji'>☄️</div>
+					</div>
+					<div className='page-background-emoji-container'>
+						<div className='page-background-emoji'>🦾</div>
+					</div>
+					<div className='page-background-emoji-container'>
+						<div className='page-background-emoji'>🧠</div>
+					</div>
+					<div className='page-background-emoji-container'>
+						<div className='page-background-emoji'>🛰️</div>
+					</div>
+					<div className='page-background-emoji-container'>
+						<div className='page-background-emoji'>🥼</div>
+					</div>
+				</div>
+				<div className='page-background-emoji-stars'>
+					<div className='page-background-emoji-star-container'>
+						<div className='page-background-emoji'>🌟</div>
+					</div>
+					<div className='page-background-emoji-star-container'>
+						<div className='page-background-emoji'>🌟</div>
+					</div>
+					<div className='page-background-emoji-star-container'>
+						<div className='page-background-emoji'>🌟</div>
+					</div>
+					<div className='page-background-emoji-star-container'>
+						<div className='page-background-emoji'>🌟</div>
+					</div>
+					<div className='page-background-emoji-star-container'>
+						<div className='page-background-emoji'>🌟</div>
+					</div>
+					<div className='page-background-emoji-star-container'>
+						<div className='page-background-emoji'>🌟</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/pages/Home/Subheadline/Subheadline.jsx
+++ src/pages/Home/Subheadline/Subheadline.jsx
@@ -7,9 +7,11 @@ export const Subheadline = () => {
 				<div className='home-subheadline-line-break' />
 				Predicted Distinction in Artificial Intelligence MSc
 				<div className='home-subheadline-line-break' />
-				Currently Working on Novel Methods for Increasing DL Interpretability and Capabilities
+				Currently Doing Mechanistic Interpretability Research
 				<div className='home-subheadline-line-break' />
 				Computer Science (Artificial Intelligence) BSc with First Class Honours
+				<div className='home-subheadline-line-break' />
+				Passionate About Building AI to Solve Our Largest Problems and to Maximise Ability for All
 			</div>
 		</div>
 	);
