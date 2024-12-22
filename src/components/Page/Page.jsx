20a4637ea6dd5d3031e62668b5ea3d133cfb3102
Daniel Davies <85223934+DanielJamesDavies@users.noreply.github.com>
2024-12-22 15:39:27 +0000

    Background Improvements

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,14 +2,29 @@ import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 
 import "./Page.css";
+import { useEffect, useState } from "react";
 
 export const Page = ({ element }) => {
+	const [hideBackground, setHideBackground] = useState(false);
+
+	useEffect(() => {
+		const getShouldHideBackground = () => {
+			const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+			const isMobile = /android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase());
+			const isIOS = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+			const isMac = /Macintosh/.test(userAgent);
+			return isMobile || isIOS || isMac;
+		};
+
+		setHideBackground(getShouldHideBackground());
+	}, [setHideBackground]);
+
 	return (
-		<div className='page'>
+		<div className={"page" + (hideBackground ? " page-background-hidden" : "")}>
 			<Header />
 			<div className='page-content'>{element}</div>
 			<Footer />
-			<div className='page-background'>
+			<div className={"page-background"}>
 				<div className='page-background-emojis'>
 					<div className='page-background-emoji-container'>
 						<div className='page-background-emoji'>🌌</div>
